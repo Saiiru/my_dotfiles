9fec6762adbee80d6a91049b6fc14cc457ae5abc
[Saiiru] <[sairufsa@gmail.com]>
2025-11-26 18:09:04 -0300

    chore(templates): remove stack scaffolds

--- templates/stacks/go-api/base/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/your-org/__PROJECT_SLUG__
-
-go 1.22
--- templates/stacks/java-api/base/mvnw.cmd
+++
@@ -1,6 +0,0 @@
-@IF EXIST "%~dp0\mvnw.cmd" (
-  "%~dp0\mvnw.cmd" %*
-) ELSE (
-  @ECHO Maven wrapper not found.
-  EXIT /B 1
-)
